ca5fb9305d68e8c11840e9e591aad69a984232de
Eddyfabbricatore <eddyfabbricatore2@gmail.com>
2024-02-08 21:28:19 +0100

    [add] file data config

---
+++ config/apartments_services_pivot.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+    [
+      'apartment_id' => '1',
+      'service_id' => '1'
+    ],
+
+    [
+      'apartment_id' => '2',
+      'service_id' => '7'
+    ],
+
+    [
+      'apartment_id' => '3',
+      'service_id' => '10'
+    ]
+];
---
+++ config/apartments_sponsors_pivot.php
@@ -0,0 +1,24 @@
+<?php
+
+return [
+    [
+      'apartment_id' => '1',
+      'sponsor_id' => '1',
+      'transaction_date' => '2023/12/12',
+      'end_sponsor_date' => '2023/12/13'
+    ],
+
+    [
+      'apartment_id' => '2',
+      'sponsor_id' => '2',
+      'transaction_date' => '2023/7/5',
+      'end_sponsor_date' => '2023/7/8'
+    ],
+
+    [
+      'apartment_id' => '3',
+      'sponsor_id' => '3',
+      'transaction_date' => '2023/10/14',
+      'end_sponsor_date' => '2023/10/21'
+    ]
+];
